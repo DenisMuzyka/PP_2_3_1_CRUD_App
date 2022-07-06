cb17f7869b36646365bdf732fc7598f51b279cb6
DenisMuzyka <demuz.prog@gmail.com>
2022-07-06 23:23:39 +0300

    task PP_2_3_1 in process

---
+++ src/main/java/web/controller/UserController.java
@@ -0,0 +1,26 @@
+package web.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import web.service.UserServiceImpl;
+
+@Controller
+@RequestMapping("/users")
+public class UserController {
+
+    private UserServiceImpl userService;
+
+    @Autowired
+    public void setUserService(UserServiceImpl userService) {
+        this.userService = userService;
+    }
+
+    @GetMapping
+    public String getAllUsers(Model model) {
+        model.addAttribute("users", userService.getAllUsers());
+        return "userslist";
+    }
+}
